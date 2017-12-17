0bf505cdb8317292d52bac15bcba6738dfd15fba
ahmedmigo <ahmad.migo@gmail.com>
2017-12-17 22:17:37 +0100

    Fixed instance issue and add nested symbols to library

--- move-to-library.sketchplugin/Contents/Sketch/moveToLibrary.js
+++ move-to-library.sketchplugin/Contents/Sketch/moveToLibrary.js
@@ -1,30 +1,27 @@
-function moveAllSymbolsToNewLibrary (context) {
-		var libraryDoc = createNewSketchFile()
-		var localSymbols = context.document.documentData().localSymbols()
-		for (var i=0; i < localSymbols.count();i ++)
-		{
-  		AddSymbolToDoc(localSymbols[i],libraryDoc.documentData(),false)
-		}
-		//addDocToLibraries(libraryDoc)
-}
-
 function moveAllSymbolsToExisitingLibrary (context) {
 	userLibraries = getAllLibraries();
 	var libraryIndex = getLayoutSettings(context,userLibraries.name)
+	var library = userLibraries.Reference[libraryIndex]
 	if (libraryIndex != -1){
+			log("⚠️")
+			var symbolsInDocByName = getSymbolsInDocByName(library.document())
+			var localSymbolsByName = getSymbolsInDocByName(context.document.documentData())
+			getIdMap(context,localSymbolsByName,symbolsInDocByName,library)
+			log(symbolsInDocByName + "⚠️")
 			var localSymbols = context.document.documentData().localSymbols()
 			for (var i = 0; i < localSymbols.count(); i++)
 			{
-				context.document.showMessage("Moving [" + i + "/" + localSymbols.count() + "] 🕗")
-				replaceInstance (context,localSymbols[i],userLibraries.Reference[libraryIndex])
+				context.api().message("Moving [" + i + "/" + localSymbols.count() + "] 🕗")
+				replaceInstance (context,localSymbols[i],symbolsInDocByName,library)
 			}
-			context.document.showMessage( movedInstancesNumber + " Re-attached ✌️" + movedSymbolsNumber + " Symbols moved to [" + userLibraries.name[libraryIndex] + "] successfully ✅ 😎 and ")
+			context.api().message( movedInstancesNumber + " Re-attached ✌️" + movedSymbolsNumber + " Symbols moved to [" + userLibraries.name[libraryIndex] + "] successfully ✅ 😎 and ")
 
 		}
 	}
 
 
 function moveSelectedSymbolsToExisitingLibrary (context){
+		log("⚠️")
 		userLibraries = getAllLibraries();
 		var libraryIndex = getLayoutSettings(context,userLibraries.name)
 		if (libraryIndex != -1){
@@ -33,18 +30,23 @@ function moveSelectedSymbolsToExisitingLibrary (context){
 				{
 					if (validate(context,i))
 					{
-						context.document.showMessage("Moving [" + i + "/" + selection.count() + "] 🕗")
+						// context.api().message("Moving [" + i + "/" + selection.count() + "] 🕗")
 						var selectedSymbols = selection[i]
+						var library = userLibraries.Reference[libraryIndex]
+						var symbolsInDocByName = getSymbolsInDocByName(library.document())
+						var localSymbolsByName = getSymbolsInDocByName(context.document.documentData())
+						getIdMap(context,localSymbolsByName,symbolsInDocByName,library)
+						log(idmap)
 						if (selectedSymbols.class() == MSSymbolMaster)
 						{
-							replaceInstance (context,selectedSymbols,userLibraries.Reference[libraryIndex])
+							replaceInstance (context,selectedSymbols,symbolsInDocByName,library)
 						} else {
-							replaceInstance (context,selectedSymbols.symbolMaster(),userLibraries.Reference[libraryIndex])
+							replaceInstance (context,selectedSymbols.symbolMaster(),symbolsInDocByName,library)
 						}
 
 					}
 				}
-				context.document.showMessage( movedInstancesNumber + " Re-attached ✌️" + movedSymbolsNumber + " Symbols moved to [" + userLibraries.name[libraryIndex] + "] successfully ✅ 😎 and ")
+				context.api().message( movedInstancesNumber + " Re-attached ✌️" + movedSymbolsNumber + " Symbols moved to [" + userLibraries.name[libraryIndex] + "] successfully ✅ 😎 and ")
 			}
 }
 
@@ -133,6 +135,7 @@ function getLayoutSettings(context,librariesArray) {
     groupFrame.addSubview(groupGranularityValue);
 		var groupGranularityDescription = createDescription('Created by Ahmed Genaidy, follow me on twitter @ser_migo',11,NSMakeRect(0,0,300,42));
 		groupFrame.addSubview(groupGranularityDescription);
+
     alertWindow.addButtonWithTitle('select ✅');
     alertWindow.addButtonWithTitle('Cancel');
 
@@ -145,10 +148,10 @@ function getLayoutSettings(context,librariesArray) {
 		var responseCode = alertWindow.runModal();
         log(responseCode)
         if (responseCode == 1000) {
-                context.document.showMessage("loading..  🕑 ")
+                // context.api().message("loading..  🕑 ")
 								return [groupGranularityValue indexOfSelectedItem]
 				} else {
-								context.document.showMessage("bye bye 👋")
+								context.api().message("bye bye 👋")
 								return -1;
 				}
 }
@@ -158,13 +161,13 @@ function getLayoutSettings(context,librariesArray) {
 function validate (context,i){
     var doc = context.document;
     if (context.selection.count() == 0) {
-        doc.showMessage("No layer selected, Please select symbol instance layer 🤔");
+        context.api().message("No layer selected, Please select symbol instance layer 🤔");
 		}
     else if (context.selection[i].class() == MSSymbolMaster || context.selection[i].class() == MSSymbolInstance) {
         return true;
     }
     else {
-        doc.showMessage("layer selected is not Symbol 🤔");
+        context.api().message("layer selected is not Symbol 🤔");
     }
     return false;
 }
@@ -177,49 +180,72 @@ var copiedSymbols = {} //Object for dublicate symbols
 /*******************Functions************************/
 
 //create New sketch file and ask user for the location of this file
-function createNewSketchFile (){
-  var newFile = MSDocument.new();
-  newFile.setFileName(context.document.fileName().replace('.sketch','') + " Library.sketch")
-  newFile.saveDocumentAs(newFile)
-  log(newFile.fileName())
-  return newFile
-}
 
 
-//Add new page to document
-function AddNewPageToDoc(name,doc){
-  var tempPage = MSPage.new();
-  doc.addPage(tempPage);
-  tempPage.setName(name);
-}
 
-
-function getMatchSymbolInDoc (symbol,doc)
+function getSymbolsInDocByName (doc)
 {
 	var docSymbols = doc.localSymbols()
+	var docSymbolsWithNames = {}
 	for (var i = 0; i < docSymbols.count();i++)
 	{
-		if(docSymbols[i].name().substring(0,100) == symbol.name().substring(0,100))
-		{
-			return docSymbols[i]
+			docSymbolsWithNames[docSymbols[i].name()] = docSymbols[i]
+	}
+	return docSymbolsWithNames
+}
+
+var idmap = {}
+
+function getIdMap (context, localSymbolsByName, foreignSymbolsByName,library)
+{
+	for (key in localSymbolsByName) {
+		if(foreignSymbolsByName[key] != undefined) {
+			foreignSymbol = context.document.localSymbolForSymbol_inLibrary(foreignSymbolsByName[key],library)
+			idmap[localSymbolsByName[key].symbolID()] = foreignSymbol.symbolID()
 		}
 	}
+}
+
+
+
+function getMatchSymbolInDoc (symbol,docSymbolsWithNames)
+{
+		if(docSymbolsWithNames[symbol.name()] != undefined)
+		{
+			return docSymbolsWithNames[symbol.name()]
+		}
 	return -1
 }
 
+// function getMatchSymbolInDoc (symbol,doc)
+// {
+// 	var docSymbols = doc.localSymbols()
+// 	for (var i = 0; i < docSymbols.count();i++)
+// 	{
+// 		if(docSymbols[i].name().substring(0,100) == symbol.name().substring(0,100))
+// 		{
+// 			return docSymbols[i]
+// 		}
+// 	}
+// 	return -1
+// }
+
 //add symbols with it's nest without dubliucate by using copiedSymbolsObject
 // Adject the postion on the doc with the same poition in the original file
-function AddSymbolToDoc (symbol,doc,exist) {
-  if (!exist)
-  {
+
+function AddSymbolToDoc (symbol,symbolsInDocByName,doc) {
     var symbolChildren = symbol.children()
     for (var i = 0; i < symbolChildren.count(); i++)
     {
-			var matchedSymbol = getMatchSymbolInDoc(symbol.children()[i],doc)
-			log ("🛑 " + matchedSymbol)
-      if (symbol.children()[i].class() == MSSymbolInstance && matchedSymbol == -1)
+			log(symbol.children()[i].class())
+			log(symbol.children()[i].class() == MSSymbolInstance)
+      if (symbol.children()[i].class() == MSSymbolInstance)
       {
-        AddSymbolToDoc(symbol.children()[i].symbolMaster(),doc,false)
+				var matchedSymbol = getMatchSymbolInDoc(symbol.children()[i].symbolMaster(),symbolsInDocByName)
+				log ("🛑 " + matchedSymbol)
+				if(matchedSymbol == -1 ) {
+					  AddSymbolToDoc(symbol.children()[i].symbolMaster(),symbolsInDocByName,doc)
+				}
       }
 			else if (symbol.children()[i].class() == MSSymbolInstance){
 				symbol.children()[i].changeInstanceToSymbol(matchedSymbol)
@@ -228,10 +254,10 @@ function AddSymbolToDoc (symbol,doc,exist) {
     var frameX = JSON.parse(JSON.stringify(symbol.frame().x()))
     var frameY = JSON.parse(JSON.stringify(symbol.frame().y()))
     doc.addSymbolMaster(symbol)
+		movedSymbolsNumber++;
     symbol.frame().setX(frameX)
     symbol.frame().setY(frameY)
     //copiedSymbols[symbol]=1
-  }
 }
 
 //add file to libraries
@@ -254,44 +280,47 @@ function getAllLibraries () {
 	return librariesNameAndReference;
 }
 
-function replaceInstance (context,symbol,library) {
+function replaceInstance (context,symbol,symbolsInDocByName,library) {
     var symbolID = symbol.symbolID()
     var symbolName = symbol.name()
     var librarySymbols = library.document().localSymbols()
-		var i = 0
-    for (i = 0; i < librarySymbols.count(); i++) {
-        if(librarySymbols[i].name().substring(0,100) == symbolName.substring(0,100)){
-				var foriegnSymbols = context.document.localSymbolForSymbol_inLibrary(librarySymbols[i],library)
+		var symbolInDoc = symbolsInDocByName[symbolName]
+    if(symbolInDoc != undefined){
+		var foriegnSymbols = context.document.localSymbolForSymbol_inLibrary(symbolInDoc,library)
 				reattachAllInstance(context,symbolID,foriegnSymbols)
-				break;
-      }
+				log ( movedInstancesNumber + " Instance Done 🤘")
+				context.api().message(movedInstancesNumber + " Instance Done 🤘"))
     }
 		//if symbol not exsit add and reattach instance
-		if (i == librarySymbols.count()) {
+		else {
 			if (symbol.class() == MSSymbolMaster) {
-				addSymbolTolibrary (symbol,library)
-				movedSymbolsNumber++;
+				addSymbolTolibrary (symbol,symbolsInDocByName,library)
+				log ( movedSymbolsNumber + " Symbols moved 🤘")
+				context.api().message(movedSymbolsNumber + " Symbols moved 🤘")
 				var foriegnSymbols = context.document.localSymbolForSymbol_inLibrary(symbol,library)
+				idmap[symbol.symbolID()] = foriegnSymbols.symbolID()
 				reattachAllInstance(context,symbolID,foriegnSymbols)
 			}
 			else if (symbol.class() == MSSymbolInstance ){
-				addSymbolTolibrary (symbol.symbolMaster(),library)
-				movedSymbolsNumber++;
+				addSymbolTolibrary (symbol.symbolMaster(),symbolsInDocByName,library)
 				var foriegnSymbols = context.document.localSymbolForSymbol_inLibrary(symbol,library)
+				idmap[symbol.symbolMaster().symbolID()] = foriegnSymbols.symbolID()
 				reattachAllInstance(context,symbolID,foriegnSymbols)
 			}
 		}
 }
-
-function addSymbolTolibrary (symbol,library){
-		var fileURL = library.locationOnDisk()
-		log(fileURL)
-
-		var document = MSDocument.new();
-		document.readDocumentFromURL_ofType_error(fileURL,"sketch", null);
-		AddSymbolToDoc(symbol,document.documentData(),false)
-		//document.documentData().addSymbolMaster(symbol)
-		[document writeToURL:fileURL ofType:"sketch" forSaveOperation:1 originalContentsURL:fileURL error:null]
+var fileURL = undefined
+var foreigndocument = undefined
+function addSymbolTolibrary (symbol,symbolsInDocByName,library){
+		if (fileURL == undefined)
+		{
+			fileURL = library.locationOnDisk()
+			log(fileURL)
+			foreigndocument = MSDocument.new();
+			foreigndocument.readDocumentFromURL_ofType_error(fileURL,"sketch", null);
+		}
+		AddSymbolToDoc(symbol,symbolsInDocByName,foreigndocument.documentData())
+		[foreigndocument writeToURL:fileURL ofType:"sketch" forSaveOperation:1 originalContentsURL:fileURL error:null]
 }
 
 var movedInstancesNumber = 0
@@ -301,10 +330,13 @@ function reattachAllInstance (context,symbolID,foreignSymbol) {
     var Instances = context.document.documentData().symbolInstancesBySymbolID()[symbolID]
 		if (Instances != null)
 		{
-			for (var i= 0 ; i < Instances.count(); i++)
+			var instanceArray = Instances.allObjects()
+			for (var i= 0 ; i < instanceArray.count(); i++)
 	    {
-					context.document.showMessage("Reconnecting [" + i + "/" + Instances.count() + "] Instances 🕗")
-	        Instances.allObjects()[i].changeInstanceToSymbol(foreignSymbol)
+					context.api().message("Reconnecting [" + i + "/" + Instances.count() + "] Instances 🕗")
+					log ("Reconnecting [" + i + "/" + Instances.count() + "] Instances 🕗")
+	        instanceArray[i].changeInstanceToSymbol(foreignSymbol)
+					instanceArray[i].updateOverridesWithObjectIDMap(idmap)
 					movedInstancesNumber++;
 	    }
 		}
